b46dc699364ad7f15482c556f1525879f07780e7
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-20 11:57:25 -0600

    create base

--- barbarian-blaster/Enemy/enemy.gd
+++ barbarian-blaster/Enemy/enemy.gd
@@ -1,7 +1,9 @@
 extends PathFollow3D
 
 ## The speed in m/s the enemy will move
-@export var speed := 10.0
+@export var speed :float = 10.0
+
+@onready var base : Base = get_tree().get_first_node_in_group("Base")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +12,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress += speed * delta
+	progress += delta * speed
+	if progress_ratio == 1.0:
+		base.take_damage()
+		queue_free()
---
+++ barbarian-blaster/base/base.gd
@@ -0,0 +1,5 @@
+extends Node3D
+class_name Base
+
+func take_damage() -> void:
+	print("damage taken by base")
