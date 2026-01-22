4196dc7168b9a12e84be3d95da544a6b2585e8b7
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-22 12:40:28 -0600

    follows enemies

--- barbarian-blaster/turret/turret.gd
+++ barbarian-blaster/turret/turret.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 var projectile = preload("res://turret/projectile.tscn")
-@onready var timer: Timer = $Timer
+
+var enemy_path : Path3D
+
 
 func _on_timer_timeout() -> void:
 	var proj = projectile.instantiate()
--- barbarian-blaster/turretmanager/turret_manager.gd
+++ barbarian-blaster/turretmanager/turret_manager.gd
@@ -2,8 +2,15 @@ extends Node3D
 class_name TurretManager
 
 @export var turret : PackedScene
+@export var enemy_path : Path3D
+
+#turn and look at an enemy
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
 
 func build_turret(position:Vector3) -> void:
 	var new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = position
+	new_turret.enemy_path = enemy_path
