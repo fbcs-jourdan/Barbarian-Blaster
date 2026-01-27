aac94b9574cad514a21961f6d6ad30c7c1e431c6
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-27 12:40:05 -0600

    add diff curve

--- barbarian-blaster/path_3d.gd
+++ barbarian-blaster/path_3d.gd
@@ -1,14 +1,16 @@
 extends Path3D
 
 @export var enemy_scene : PackedScene
+@export var difficulty_manager : Node
 @onready var timer: Timer = $Timer
+
+
 
 func spawn_enemy() -> void:
 	var enemy = enemy_scene.instantiate()
 	add_child(enemy)
-	
-	
-	
+	timer.wait_time = difficulty_manager.get_spawn_time()
+	enemy.max_health = difficulty_manager.get_enemy_health()
 
 
 func _on_timer_timeout() -> void:
