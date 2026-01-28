070c6b5f637c6c5b15b77b669d6b7c504397a4de
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-28 13:07:53 -0600

    add ui

--- barbarian-blaster/difficulty_manager/difficulty_manager.gd
+++ barbarian-blaster/difficulty_manager/difficulty_manager.gd
@@ -6,6 +6,8 @@ class_name DifficultyManager
 @export var enemy_heath_curve : Curve
 @onready var timer: Timer = $Timer
 
+signal stop_spawning_enemies
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.start(game_length)
@@ -24,3 +26,6 @@ func get_spawn_time() -> float:
 
 func get_enemy_health() -> float:
 	return enemy_heath_curve.sample(game_progress_ratio())
+
+func _on_timer_timeout() -> void:
+	stop_spawning_enemies.emit()
--- barbarian-blaster/path_3d.gd
+++ barbarian-blaster/path_3d.gd
@@ -1,8 +1,10 @@
 extends Path3D
 
+@export var victory_layer : CanvasLayer
 @export var enemy_scene : PackedScene
 @export var difficulty_manager : Node
 @onready var timer: Timer = $Timer
+
 
 
 
@@ -12,7 +14,18 @@ func spawn_enemy() -> void:
 	timer.wait_time = difficulty_manager.get_spawn_time()
 	enemy.max_health = difficulty_manager.get_enemy_health()
 	add_child(enemy)
+	enemy.tree_exited.connect(enemy_defeated)
 
+func enemy_defeated() -> void:
+	if timer.is_stopped():
+		for child in get_children():
+			if child is Enemy:
+				return
+		victory_layer.visible = true
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
+
+
+func _on_difficulty_manager_stop_spawning_enemies() -> void:
+	timer.stop()
