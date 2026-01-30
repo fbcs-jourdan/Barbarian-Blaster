92ea5d66968f2652f28faa6064a0529c7a0b9e43
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-30 12:36:51 -0600

    finish game

--- barbarian-blaster/Enemy/enemy.gd
+++ barbarian-blaster/Enemy/enemy.gd
@@ -25,7 +25,7 @@ var health : int :
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health = max_health
-	#Engine.time_scale = 5
+	Engine.time_scale = 5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
